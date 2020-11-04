625987c1d0ee0a6b6385d7610134d3e3a6f3bd21
xnoidnxd <dionisiusyogase@gmail.com>
2020-11-04 20:00:16 +0700

    test error message

--- app/src/main/java/com/dicoding/mysimplecleanarchitecture/presentation/MainActivity.kt
+++ app/src/main/java/com/dicoding/mysimplecleanarchitecture/presentation/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         val factory = MainViewModelFactory.getInstance()
         val viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
 
-        viewModel.setName("Dico")
+        viewModel.setName("Dicod")
         viewModel.message.observe(this, Observer {
             tv_welcome.text = it.welcomeMessage
         })
